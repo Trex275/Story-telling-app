27f430209ca873ab5e2103d858f35dbf31e3c362
Trex275 <73885729+Trex275@users.noreply.github.com>
2021-08-11 16:59:43 +0530

    Add files via upload

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,13 +1,56 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import Firebase from "firebase";
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf")
+};
 
 export default class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fontsLoaded: false,
+      isEnabled: false,
+      light_theme: true,
+      profile_image: "",
+      name: ""
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+    this.fetchUser();
+  }
+
+  async fetchUser(){
+    var theme, name, Image
+    await firebase.database().ref("/users/" + firebase.auth().currentUser.uid)
+    .on("value", function(snapshot){
+      theme = snapshot.val().current_theme
+      name = `${snapshot.val().first_name} ${snapshot.val().last_name}`
+      image= snapshot.val().profile_picture
+    })
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Profile</Text>
-      </View>
-    );
+    if (!this.state.fontsLoaded) {
+      return <AppLoading />
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text>Profile</Text>
+        </View>
+      );
+    }
   }
 }
 

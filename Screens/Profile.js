71c1785f5b5bce8f42823d0d27ca70d22d4394bc
Trex275 <73885729+Trex275@users.noreply.github.com>
2021-08-17 16:54:18 +0530

    Add files via upload

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Switch, Image, Platform } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
@@ -25,19 +25,32 @@ export default class Profile extends Component {
     await Font.loadAsync(customFonts);
     this.setState({ fontsLoaded: true });
   }
-
+  toggleSwitch (){
+    const previous_state = this.state.isEnabled;
+    const theme = !this.state.isEnabled ? "dark" : "light";
+    var updates =  {};
+    updates ["/users/" + firebase.auth().currentUser.uid + "/current_theme"] = theme;
+    firebase.database().ref().update(updates);
+    this.setState({isEnabled: !previous_state, light_theme: previous_state})
+  }
   componentDidMount() {
     this._loadFontsAsync();
     this.fetchUser();
   }
 
-  async fetchUser(){
+  async fetchUser() {
     var theme, name, Image
     await firebase.database().ref("/users/" + firebase.auth().currentUser.uid)
-    .on("value", function(snapshot){
-      theme = snapshot.val().current_theme
-      name = `${snapshot.val().first_name} ${snapshot.val().last_name}`
-      image= snapshot.val().profile_picture
+      .on("value", function (snapshot) {
+        theme = snapshot.val().current_theme
+        name = `${snapshot.val().first_name} ${snapshot.val().last_name}`
+        image = snapshot.val().profile_picture
+      });
+    this.setState({
+      light_theme: theme === "light" ? true : false,
+      isEnabled: theme === "light" ? false : true,
+      name: name,
+      profile_image: image
     })
   }
 
@@ -46,8 +59,41 @@ export default class Profile extends Component {
       return <AppLoading />
     } else {
       return (
-        <View style={styles.container}>
-          <Text>Profile</Text>
+        <View style={this.state.light_theme ? styles.containerLight : styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+              <Image
+                source={require("../assets/logo.png")}
+                style={styles.iconImage}
+              ></Image>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text
+                style={this.state.light_theme ? styles.appTitleTextLight : styles.appTitleText
+                }
+              >
+                Storytelling App
+              </Text>
+            </View>
+          </View>
+          <View style={styles.styleContainer}>
+            <View style={styles.profileImageContainer}>
+              <Image source={{ uri: this.state.profile_image }} style={styles.profileImage}> </Image>
+              <Text style={this.state.light_theme ? styles.nameTextLight : styles.nameText}>{this.state.name}</Text>
+            </View>
+          </View>
+          <View styles={style.themeContainer}>
+            <Text style={this.state.light_theme ? styles.themeTextLight : styles.themeText}>Dark Theme</Text>
+            <Switch onValueChange={() => { this.toggleSwitch() }}
+              value={this.state.isEnabled}
+              style = {{transform : [{scaleX: 1.3}, {scaleY: 1.3}]}}
+              trackColor = {{
+                false: "#767676",
+                true : this.state.light_theme ? "#eee" : "white"
+              }} 
+              thumbColor = {this.state.isEnabled ? "#ee7678" : "#f4f4f4"}/>
+          </View>
         </View>
       );
     }
@@ -57,7 +103,86 @@ export default class Profile extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#15193c"
+  },
+  containerLight: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  droidSafeArea: {
+    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35)
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row"
+  },
+  appIcon: {
+    flex: 0.3,
     justifyContent: "center",
     alignItems: "center"
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center"
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  appTitleTextLight: {
+    color: "black",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans"
+  },
+  screenContainer: {
+    flex: 0.85
+  },
+  profileImageContainer: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  profileImage: {
+    width: RFValue(140),
+    height: RFValue(140),
+    borderRadius: RFValue(70)
+  },
+
+  nameText: {
+    color: "white",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans",
+    marginTop: RFValue(10)
+  },
+  nameTextLight: {
+    color: "black",
+    fontSize: RFValue(40),
+    fontFamily: "Bubblegum-Sans",
+    marginTop: RFValue(10)
+  },
+  themeContainer: {
+    flex: 0.2,
+    flexDirection: "row",
+    justifyContent: "center",
+    marginTop: RFValue(20)
+  },
+  themeText: {
+    color: "white",
+    fontSize: RFValue(30),
+    fontFamily: "Bubblegum-Sans",
+    marginRight: RFValue(15)
+  },
+  themeTextLight: {
+    color: "black",
+    fontSize: RFValue(30),
+    fontFamily: "Bubblegum-Sans",
+    marginRight: RFValue(15)
   }
 });
+
